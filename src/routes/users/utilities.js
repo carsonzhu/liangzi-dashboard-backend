8697dcf7fe9aaf77d1cc4f181f5f6479b534c5ec
Edward Feng <edward0414@gmail.com>
2019-07-08 10:03:16 -0400

    init file

---
+++ src/routes/users/utilities.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import UserModel from "../../models/user";
+
+const fetchUsers = () => {};
+
+const createUser = ({}) => {};
+
+const editUser = ({ userId, fieldToUpdate }) => {};
+
+const suspendUser = ({ userId }) => {};
+
+export { fetchUsers, createUser, editUser, suspendUser };

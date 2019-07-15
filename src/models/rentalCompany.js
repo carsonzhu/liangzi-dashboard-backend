0d15400621932b835cf852c1a8ef7f306475a7bb
Edward Feng <edward0414@gmail.com>
2019-07-15 13:27:15 -0400

    wip..

---
+++ src/models/rentalCompany.js
@@ -0,0 +1,39 @@
+"use strict";
+
+import mongoose, { Schema } from "mongoose";
+import { validateUrl, localeObjectValidation } from "../utilities/validations";
+
+const RentalCompanySchema = new Schema({
+  name: {
+    type: mongoose.Schema.Types.Mixed,
+    validator: localeObjectValidation,
+    required: true,
+    unique: true
+  },
+  address: {
+    type: mongoose.Schema.Types.Mixed,
+    validator: localeObjectValidation,
+    required: true,
+    unique: true
+  },
+  image: {
+    type: String,
+    validator: validateUrl,
+    unique: true
+  },
+  rating: {
+    type: Number
+  },
+  perks: {
+    type: [mongoose.Schema.Types.Mixed],
+    validator: localeObjectValidation
+  },
+  locationAlias: {
+    type: [mongoose.Schema.Types.Mixed],
+    validator: localeObjectValidation
+  }
+});
+
+const myDB = mongoose.connection.useDb(process.env.WEB_BACKEND_DB);
+
+export default myDB.model("rental_companies", RentalCompanySchema);
